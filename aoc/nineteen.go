77c642482c156107d1f306ea0d929994d45d950c
Heli Villarreal <heli.v@pm.me>
2024-12-19 10:32:01 -0600

    Solved 19 part 1 (DP)

--- aoc/nineteen.go
+++ aoc/nineteen.go
@@ -10,13 +10,23 @@ import (
 
 func Nineteen(lines []string) int {
 	options := util.ParseStringArray(lines[0])
+	optMap := make(map[rune][]string)
+	memo := make(map[string]bool)
+	for _, option := range options {
+		r := rune(option[0])
+		_, ok := optMap[r]
+		if !ok {
+			optMap[r] = make([]string, 0)
+		}
+		optMap[r] = append(optMap[r], option)
+	}
 	sum := 0
 	for i, s := range lines[2:] {
 		if len(s) == 0 {
 			continue
 		}
 		t1 := time.Now()
-		if combinationPossible(s, &options) {
+		if combinationPossible(s, &optMap, &memo) {
 			sum += 1
 		}
 		log.Println(strconv.Itoa(i), " solved after ", time.Now().Nanosecond()-t1.Nanosecond(), "ns")
@@ -24,16 +34,26 @@ func Nineteen(lines []string) int {
 	return sum
 }
 
-func combinationPossible(s string, options *[]string) bool {
+func combinationPossible(s string, optMap *map[rune][]string, memo *map[string]bool) bool {
 	if len(s) == 0 {
 		return true
 	}
+	options, ok := (*optMap)[rune(s[0])]
+	if !ok {
+		return false
+	}
+	val, inMemo := (*memo)[s]
+	if inMemo {
+		return val
+	}
 
-	for _, opt := range *options {
+	for _, opt := range options {
 		newS, found := strings.CutPrefix(s, opt)
-		if found && combinationPossible(newS, options) {
+		if found && combinationPossible(newS, optMap, memo) {
+			(*memo)[s] = true
 			return true
 		}
 	}
+	(*memo)[s] = false
 	return false
 }
